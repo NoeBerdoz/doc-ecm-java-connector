9e4248ca3ba6a93cbb56e91deae09496ea41fc57
NoeBerdoz <berdoznoe@gmail.com>
2025-05-27 21:06:25 +0200

    remove unused attribute

--- src/main/java/ch/nb/service/ApiService.java
+++ src/main/java/ch/nb/service/ApiService.java
@@ -24,10 +24,6 @@ public class ApiService {
     private static final PropertiesLoader propertiesLoader = new PropertiesLoader("api.properties");
     private static final String API_BASE_URL = propertiesLoader.getProperty("api.url");
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .connectTimeout(Duration.ofSeconds(10))
-            .build();
 
     private static void getAuthentication() {
         if (DocEcmApiAuth.currentToken == null || DocEcmApiAuth.isTokenExpired(60)) {
